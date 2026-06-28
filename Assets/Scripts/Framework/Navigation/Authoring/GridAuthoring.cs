4abb77aee20ad170e335ae9efeb3ebc86694182d
SL_Skuy <3063473749@qq.com>
2026-06-28 22:32:44 +0800

    调整文件结构及注释

--- Assets/Scripts/Framework/Navigation/Authoring/GridAuthoring.cs
+++ Assets/Scripts/Framework/Navigation/Authoring/GridAuthoring.cs
@@ -62,9 +62,6 @@ namespace Framework
                     MaxComputePerFrame = authoring.maxComputePerFrame
                 };
                 AddComponent(gridEntity, config);
-                
-                // 添加寻路请求队列缓冲
-                AddBuffer<ASOperationsBuffer>(gridEntity);
             }
         }
     }
