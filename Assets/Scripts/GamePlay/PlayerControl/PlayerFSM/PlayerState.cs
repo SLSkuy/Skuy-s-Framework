6016bfdca040e15fc0d4ac27c6ecbde2bb50da5b
SL_Skuy <3063473749@qq.com>
2026-08-09 02:23:26 +0800

    添加动作

---
+++ Assets/Scripts/GamePlay/PlayerControl/PlayerFSM/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿namespace GamePlay.EntitySystem
+{
+    /// <summary>
+    /// 玩家状态
+    /// </summary>
+    public enum PlayerState
+    {
+        Idle,
+        Walking,
+        Running,
+        Sprinting,
+        Jumping,
+        Falling,
+    }
+}
